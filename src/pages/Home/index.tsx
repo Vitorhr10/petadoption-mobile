9f69e37c0c0f9843c1d46d388fe01b667219d82d
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2021-09-08 00:03:13 -0300

    feat: Add fonts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,103 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 
 const Home = () => {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <Image source={require('../../assets/logo.png')} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32,
+  },
+
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+
+  title: {
+    color: '#564556',
+    fontSize: 40,
+    fontFamily: 'FredokaOne_400Regular',
+    maxWidth: 260,
+    marginTop: 72,
+  },
+
+  description: {
+    color: '#6C6C80',
+    fontSize: 16,
+    marginTop: 16,
+    fontFamily: 'Comfortaa_400Regular',
+    maxWidth: 260,
+    lineHeight: 28,
+  },
+
+  footer: {},
+
+  select: {},
+
+  input: {
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    marginBottom: 8,
+    paddingHorizontal: 24,
+    fontSize: 16,
+  },
+
+  button: {
+    backgroundColor: '#C67472',
+    height: 60,
+    flexDirection: 'row',
+    borderRadius: 10,
+    overflow: 'hidden',
+    alignItems: 'center',
+    marginTop: 8,
+  },
+
+  buttonIcon: {
+    height: 60,
+    width: 60,
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    flex: 1,
+    justifyContent: 'center',
+    textAlign: 'center',
+    color: '#FFF',
+    fontFamily: 'Comfortaa_500Medium',
+    fontSize: 24,
+    fontWeight: 'bold'
+  }
+});
+
+const pickerSelectStyles = StyleSheet.create({
+  viewContainer: {
+    borderWidth: 1,
+    borderColor: '#fff',
+    borderRadius: 8,
+    marginBottom: 8,
+    backgroundColor: '#fff',
+    paddingHorizontal: 10,
+    paddingVertical: 2,
+    height: 56,
+  },
+  iconContainer: {
+    top: 18,
+    right: 12,
+    color: '#A0A0B2',
+    opacity: 0.5
+  },
+});
 
 export default Home;
