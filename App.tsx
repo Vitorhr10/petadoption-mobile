b4e0166d1c684886ed0687a3d0152f542eec43f1
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2021-10-12 23:40:14 -0300

    -- feat: Detail and PetPoints screens

--- App.tsx
+++ App.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import AppLoading from 'expo-app-loading';
-import { StatusBar } from 'react-native';
-
-import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
-import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
-
-import Home from './src/pages/Home';
+import { AppLoading } from 'expo'
+import { StatusBar, View } from 'react-native';
+import { Comfortaa_400Regular, Comfortaa_500Medium, Comfortaa_600SemiBold, Comfortaa_700Bold } from '@expo-google-fonts/comfortaa'
+import { FredokaOne_400Regular, useFonts } from '@expo-google-fonts/fredoka-one'
+import Routes from './src/routes'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    Roboto_400Regular, 
-    Roboto_500Medium, 
-    Ubuntu_700Bold
-  });
+    Comfortaa_400Regular,
+    Comfortaa_500Medium,
+    Comfortaa_700Bold,
+    Comfortaa_600SemiBold,
+    FredokaOne_400Regular
+  })
 
   if (!fontsLoaded) {
     return <AppLoading />
   }
-  
+
   return (
     <>
-      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
-      <Home />
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+      <Routes />
     </>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,109 @@
-import React from 'react';
-import { View, Image, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { Feather as Icon } from '@expo/vector-icons'
+import { View, ImageBackground, Image, StyleSheet, Text, TextInput } from 'react-native'
+import { RectButton } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native'
+import RNPickerSelect from 'react-native-picker-select'
+import axios from 'axios'
 
 const Home = () => {
+  const navigation = useNavigation()
+
+  const [ufs, setUfs] = useState<PickerItem[]>([]);
+  const [cities, setCities] = useState<PickerItem[]>([]);
+  const [selectedUf, setSelectedUf] = useState(null);
+  const [selectedCity, setSelectedCity] = useState(null);
+
+  function handleNavigateToPetPoints() {
+    navigation.navigate('PetPoints', {
+      uf: selectedUf,
+      city: selectedCity
+    })
+  }
+
+  interface IBGEUFResponse {
+    sigla: string;
+    nome: string;
+  }
+
+  interface IBGECityResponse {
+    nome: string;
+  }
+
+  interface PickerItem {
+    label: string;
+    value: string;
+  }
+
+  useEffect(() => {
+    axios.get<IBGEUFResponse[]>('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome')
+      .then(response => {
+        const ufInitials = response.data.map(uf => ({
+          label: uf.nome,
+          value: uf.sigla,
+        }));
+        setUfs(ufInitials);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (selectedUf === null) return;
+
+    axios.get<IBGECityResponse[]>(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`)
+      .then(response => {
+        const cityNames = response.data.map(city => ({
+          label: city.nome,
+          value: city.nome,
+        }));
+        setCities(cityNames);
+      });
+  }, [selectedUf]);
+
   return (
-    <View style={styles.container}>
-      <Image source={require('../../assets/logo.png')} />
-    </View>
-  );
-};
+    <ImageBackground
+      source={require('../../assets/home-background.png')}
+      style={styles.container}
+      imageStyle={{ width: 91, height: 549, left: 300, top: 30 }}
+    >
+      <View style={styles.main}>
+        <Image source={require('../../assets/logo.png')} />
+        <Text style={styles.title}>O lugar ideal para encontrar seu novo pet.</Text>
+        <Text style={styles.description}>Ajudamos pessoas a encontrarem animais de estimação disponíveis para adoção de forma fácil e direta.</Text>
+      </View>
+
+      <View style={styles.footer}>
+
+        <RNPickerSelect
+          style={pickerSelectStyles}
+          onValueChange={(value) => setSelectedUf(value)}
+          placeholder={{ label: "Selecione um estado", value: null }}
+          items={ufs}
+          Icon={() => <Icon name="chevron-down" size={20} color="#A0A0B2" />}
+        />
+
+        <RNPickerSelect
+          style={pickerSelectStyles}
+          onValueChange={(value) => setSelectedCity(value)}
+          placeholder={{ label: "Selecione uma cidade", value: null }}
+          items={cities}
+          disabled={cities.length === 0}
+          Icon={() => <Icon name="chevron-down" size={20} color="#A0A0B2" />}
+        />
+
+        <RectButton style={styles.button} onPress={handleNavigateToPetPoints}>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="arrow-right" color="#FFF" size={24} />
+            </Text>
+          </View>
+          <Text style={styles.buttonText}>
+            Entrar
+          </Text>
+        </RectButton>
+      </View>
+    </ImageBackground>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -100,4 +196,4 @@ const pickerSelectStyles = StyleSheet.create({
   },
 });
 
-export default Home;
+export default Home
