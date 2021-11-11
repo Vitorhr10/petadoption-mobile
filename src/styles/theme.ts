89faa53b798b127b1acddabd217a26d6604e9ecd
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2021-11-10 23:40:53 -0300

    feat: Add app theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+  colors: {
+    background: '#1C1D26',
+    primary: '#7350F2',
+    secondary: '#d11000',
+    text: '#FFFFFF',
+    shape: '#4E4E5D',
+    note: '#4E4E5D'
+  },
+
+  fonts: {
+    regular: 'Comfortaa_400Regular',
+    medium: 'Comfortaa_400Regular',
+    bold: 'Comfortaa_700Bold'
+  }
+}
